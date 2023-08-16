b4774b690dd0fcade85bd32792baeef8cf2e00d4
SlendyGamer <gabrielcarotti@hotmail.com>
2023-08-15 21:42:53 -0300

    mult div
    funções de multiplicação e divisão adicionadas

--- calculadora v0.c
+++ calculadora v0.c
@@ -21,12 +21,18 @@ float subtract(float N1)
 
 float multiply(float N1)
     {
-
+        float N2 = 0, Res;
+        scanf("%f", &N2);
+        Res = N1 * N2;
+        return Res;
     }
 
 float divide(float N1)
     {
-
+        float N2 = 0, Res;
+        scanf("%f", &N2);
+        Res = N1 / N2;
+        return Res;
     }
 
 int main()
